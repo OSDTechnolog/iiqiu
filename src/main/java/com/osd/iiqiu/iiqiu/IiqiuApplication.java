bd4462ec1f2938f73d595c7d4054b734d646bc66
shichaofan <2361115313@qq.com>
2018-07-16 23:52:08 +0800

    iiqing代码创建

---
+++ src/main/java/com/osd/iiqiu/iiqiu/IiqiuApplication.java
@@ -0,0 +1,12 @@
+package com.osd.iiqiu.iiqiu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IiqiuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IiqiuApplication.class, args);
+	}
+}
